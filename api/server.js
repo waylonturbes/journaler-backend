c0c85e49146be0da8d7ee6593dac5f532ef49d6e
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-01-17 21:13:53 -0800

    Initialized authRouter

--- api/server.js
+++ api/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
+const authRouter = require("./routers/authRouter")
 
 const server = express()
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+
+// Routes
+server.use("/api/auth", authRouter)
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Journaler API" })
