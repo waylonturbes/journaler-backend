3b6f5303d9ee5ffe8093fdf50c72440581938511
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-01-17 21:27:54 -0800

    Added new endpoint to authRouter

--- api/routers/authRouter.js
+++ api/routers/authRouter.js
@@ -1,3 +1,13 @@
 const router = require("express").Router()
 
+router.all("/*", async (req, res, next) => {
+  try {
+    res.status(200).json({
+      message: "Please [POST] to either /api/auth/login or /api/auth/register"
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
 module.exports = router
