28ef419c8aecde3257156e8084845bfc569b1dfb
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-01-17 21:05:32 -0800

    Added more user seed data

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -10,6 +10,27 @@ const users = [
     email: "joe.smith@genericnames.net",
     first_name: "Joe",
     last_name: "Smith"
+  },
+  {
+    username: "AleskiPetrov",
+    password: password,
+    email: "aleski.petrov@genericnames.net",
+    first_name: "",
+    last_name: ""
+  },
+  {
+    username: "barney11",
+    password: password,
+    email: "barney11@genericnames.net",
+    first_name: "Barney",
+    last_name: ""
+  },
+  {
+    username: "amanda99",
+    password: password,
+    email: "amanda99@genericnames.net",
+    first_name: "",
+    last_name: "Jackson"
   }
 ]
 
