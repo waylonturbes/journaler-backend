647034b8bd1af4f8b0dfa855e5f792c12527eeae
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-01-23 11:07:32 -0800

    feat: new auth tests

--- api/routers/authRouter.test.js
+++ api/routers/authRouter.test.js
@@ -14,45 +14,58 @@ afterAll(async () => {
 });
 
 describe("[POST] /api/auth/register", () => {
-  it('if request body is valid, respond with the message "Successfully registered {username}" and status code 201', async () => {
-    const expectedMessage = /successfully registered/i;
+  it("if request body is valid, respond with the new user object and status code 201", async () => {
+    const expectedResponse = {
+      username: "frodo",
+      email: "fordo@shire.me",
+    };
     const res = await request(server).post("/api/auth/register").send({
       username: "frodo",
       email: "fordo@shire.me",
       password: "1234",
     });
     expect(res.status).toBe(201);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.body).toMatchObject(expectedResponse);
   });
   it('if request body lacks an email, respond with the message "Email address is required" and status code 400', async () => {
-    const expectedMessage = /email address is required/i;
+    const expectedResponse = /email address is required/i;
     const res = await request(server).post("/api/auth/register").send({
       username: "frodo",
       email: "",
       password: "1234",
     });
     expect(res.status).toBe(400);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.body.message).toMatch(expectedResponse);
   });
-  it('if the request body\'s username is taken, respond with the message "Username {username} is taken" and status code 409', async () => {
-    const expectedMessage = /is taken/i;
+  it('if request body contains an invalid email, respond with the message "Invalid email address" and status code 400', async () => {
+    const expectedResponse = /invalid email address/i;
     const res = await request(server).post("/api/auth/register").send({
-      username: "joe_smith",
-      email: "smith@joe.imp",
+      username: "frodo",
+      email: "badEmail",
       password: "1234",
     });
-    expect(res.status).toBe(409);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.status).toBe(400);
+    expect(res.body.message).toMatch(expectedResponse);
   });
   it('if the request body\'s email is taken, respond with the message "Email {email} is taken" and status code 409', async () => {
-    const expectedMessage = /is taken/i;
+    const expectedResponse = /is taken/i;
     const res = await request(server).post("/api/auth/register").send({
       username: "smith_joe",
       email: "joe.smith@genericnames.net",
       password: "1234",
     });
     expect(res.status).toBe(409);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.body.message).toMatch(expectedResponse);
+  });
+  it('if the request body\'s username is taken, respond with the message "Username {username} is taken" and status code 409', async () => {
+    const expectedResponse = /is taken/i;
+    const res = await request(server).post("/api/auth/register").send({
+      username: "joe_smith",
+      email: "smith@joe.imp",
+      password: "1234",
+    });
+    expect(res.status).toBe(409);
+    expect(res.body.message).toMatch(expectedResponse);
   });
   it("if both the request body's username & email are taken, respond with the message object containing both their error messages and status code 409", async () => {
     const res = await request(server).post("/api/auth/register").send({
@@ -65,23 +78,23 @@ describe("[POST] /api/auth/register", () => {
     expect(res.body.message).toHaveProperty("email");
   });
   it('if the request body lacks a username, respond with the message "Username is required" and status code 400', async () => {
-    const expectedMessage = /username is required/i;
+    const expectedResponse = /username is required/i;
     const res = await request(server).post("/api/auth/register").send({
       username: "",
       email: "fordo@shire.me",
       password: "1234",
     });
     expect(res.status).toBe(400);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.body.message).toMatch(expectedResponse);
   });
   it('if the request body lacks a password, respond with the message "Password is required" and status code 400', async () => {
-    const expectedMessage = /password is required/i;
+    const expectedResponse = /password is required/i;
     const res = await request(server).post("/api/auth/register").send({
       username: "frodo",
       email: "fordo@shire.me",
       password: "",
     });
     expect(res.status).toBe(400);
-    expect(res.body.message).toMatch(expectedMessage);
+    expect(res.body.message).toMatch(expectedResponse);
   });
 });
