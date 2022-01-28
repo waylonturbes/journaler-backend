20d4272f4aaa2c768280a7ac93b0ea192a08a75f
Waylon Turbes <waylonturbes@users.noreply.github.com>
2022-01-27 21:04:55 -0800

    feat: getBy and add journal model functions

--- api/models/journalsModel.js
+++ api/models/journalsModel.js
@@ -14,6 +14,30 @@ async function getAll() {
   return journals;
 }
 
+async function getBy(filter) {
+  const journal = await db("journals")
+    .select("journal_id", "title", "journal_entry", "created_at", "updated_at")
+    .where(filter)
+    .orderBy("journal_id");
+  return journal;
+}
+
+async function add(journalInfo) {
+  const [newJournal] = await db("journals")
+    .insert(journalInfo)
+    .returning([
+      "journal_id",
+      "user_id",
+      "title",
+      "journal_entry",
+      "created_at",
+      "updated_at",
+    ]);
+  return newJournal;
+}
+
 module.exports = {
   getAll,
+  getBy,
+  add,
 };
